a4f67de16f4f20864420d2c22cf3827ac308c557
Kars Rao <kars@predikkta.com>
2023-08-08 21:30:50 +1000

    collection grid gets categories from object

--- src/layout/CollectionGrid.js
+++ src/layout/CollectionGrid.js
@@ -1,26 +1,24 @@
 import { Link } from "react-router-dom"
 import "./CollectionGrid.css"
 
+import productData from "../productData.js"
+
 import { Grid } from "@mui/material"
 
 function CollectionGrid({ setSelectedCollection }){
+    const catItems = [...new Set(productData.map((val) => val.category))];
+
     return(
         <Grid container spacing={4} className="" justifyContent="center" alignItems="center">
-            <Grid item xs={12} md={4}>
-                <div className="collection-tile">
-                    <Link to="/products/collars" onClick={() => setSelectedCollection("collars")}>Shop Collars</Link>
-                </div>                    
-            </Grid>
-            <Grid item xs={12} md={4}>
-                <div className="collection-tile">
-                    <Link to="/products/leashes" onClick={() => setSelectedCollection("leashes")}>Shop Leashes</Link>
-                </div>
-            </Grid>
-            <Grid item xs={12} md={4}>
-                <div className="collection-tile">
-                    <Link to="/products/bandanas" onClick={() => setSelectedCollection("bandanas")}>Shop Bandanas</Link>
-                </div>
-            </Grid>
+            {catItems.map((cat) => {
+                return(
+                    <Grid item xs={12} md={4}>
+                        <div className="collection-tile">
+                            <Link to={"/products/"+cat} onClick={() => setSelectedCollection(cat)}>Shop {cat}</Link>
+                        </div>                    
+                    </Grid>
+                )                    
+            })}
         </Grid>
     )
 }
