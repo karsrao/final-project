1787686f23f0799bcf97a187a2ade600972d29aa
Kars Rao <kars@predikkta.com>
2023-08-10 20:07:20 +1000

    added more detail to productdata

--- src/productData.js
+++ src/productData.js
@@ -2,7 +2,7 @@ const productData = [
     {
         "sku": "SKU001",
         "productName": "Classic Paisley Bandana",
-        "productDescription": "Timeless paisley pattern bandana for a versatile look.",
+        "productDescription": "Add a touch of timeless style to your furry friend's look with this Classic Paisley Bandana. Featuring an intricate paisley pattern, this bandana offers a versatile and stylish accessory that complements any occasion. Crafted with high-quality materials for comfort and durability. Suitable for small to medium-sized dogs. Size: 21\" x 21\".",
         "productPrice": "$9.99",
         "productImage": "paisley-bandana.jpg",
         "category": "bandanas",
@@ -11,7 +11,7 @@ const productData = [
     {
         "sku": "SKU004",
         "productName": "Floral Bandana",
-        "productDescription": "Delicate floral pattern bandana for a feminine look.",
+        "productDescription": "Elevate your pet's elegance with our Floral Bandana. This delicate floral pattern bandana adds a touch of femininity to your furry friend's ensemble. Made from soft and breathable fabric, it ensures comfort and style. Suitable for dogs of all sizes. Size: 18\" x 18\".",
         "productPrice": "$7.99",
         "productImage": "floral-bandana.jpg",
         "category": "bandanas",
@@ -20,7 +20,7 @@ const productData = [
     {
         "sku": "SKU006",
         "productName": "Vintage Paisley Bandana",
-        "productDescription": "Retro-style paisley pattern bandana for a classic vibe.",
+        "productDescription": "Step back in time with our Vintage Paisley Bandana. The retro-style paisley pattern brings a classic vibe to your pet's wardrobe. Crafted from premium fabric, this bandana offers both style and comfort. Ideal for dogs with discerning tastes. Size: 24\" x 24\".",
         "productPrice": "$11.99",
         "productImage": "vintage-bandana.jpg",
         "category": "bandanas",
@@ -29,7 +29,7 @@ const productData = [
     {
         "sku": "SKU011",
         "productName": "Red Plaid Bandana",
-        "productDescription": "Classic red plaid bandana for a timeless look.",
+        "productDescription": "Classic red plaid pattern bandana for a timeless look. Add a pop of color and style to your pet's outfit. Made from high-quality fabric for comfort and durability. Suitable for dogs of all sizes. Size: 20\" x 20\".",
         "productPrice": "$9.99",
         "productImage": "red_plaid_bandana.jpg",
         "category": "bandanas",
@@ -38,7 +38,7 @@ const productData = [
     {
         "sku": "SKU012",
         "productName": "Pokemon Bandana",
-        "productDescription": "Fun Pokemon-themed bandana for fans of the franchise.",
+        "productDescription": "Calling all Pokemon fans! Our Pokemon-themed bandana is a must-have accessory for your pet. Show off your love for the franchise with this fun and vibrant design. Made from soft and breathable fabric for your pet's comfort. Suitable for dogs of all sizes. Size: 22\" x 22\".",
         "productPrice": "$14.99",
         "productImage": "pokemon_bandana.jpg",
         "category": "bandanas",
@@ -47,7 +47,7 @@ const productData = [
     {
         "sku": "SKU013",
         "productName": "Mexican Fiesta Bandana",
-        "productDescription": "Vibrant Mexican-themed bandana for a festive look.",
+        "productDescription": "Get ready to fiesta with our Mexican-themed bandana! This vibrant and festive accessory is perfect for celebrating special occasions or adding some flair to your pet's look. Made from high-quality fabric for comfort and style. Suitable for dogs of all sizes. Size: 19\" x 19\".",
         "productPrice": "$11.99",
         "productImage": "mexican_bandana.jpg",
         "category": "bandanas",
@@ -56,7 +56,7 @@ const productData = [
     {
         "sku": "SKU014",
         "productName": "Green Spotted Bandana",
-        "productDescription": "Unique green spotted pattern bandana for a playful look.",
+        "productDescription": "Stand out from the pack with our Green Spotted Bandana. This unique and playful pattern adds a touch of fun to your pet's attire. Made from soft and breathable fabric, it ensures comfort and style. Suitable for dogs of all sizes. Size: 17\" x 17\".",
         "productPrice": "$8.99",
         "productImage": "green_spotted_bandana.jpg",
         "category": "bandanas",
@@ -65,7 +65,7 @@ const productData = [
     {
         "sku": "SKU015",
         "productName": "Orange Fruit Bandana",
-        "productDescription": "Fresh orange fruit pattern bandana for a fruity vibe.",
+        "productDescription": "Freshen up your pet's look with our Orange Fruit Bandana. The fruity pattern adds a burst of color and energy to your furry friend's ensemble. Made from high-quality fabric for comfort and durability. Suitable for dogs of all sizes. Size: 16\" x 16\".",
         "productPrice": "$9.99",
         "productImage": "orange_fruit_bandana.jpg",
         "category": "bandanas",
@@ -74,7 +74,7 @@ const productData = [
     {
         "sku": "SKU016",
         "productName": "What's Shakin Bacon Bandana",
-        "productDescription": "Whimsical 'What's Shakin Bacon' bandana for a fun twist.",
+        "productDescription": "Add a playful twist to your pet's style with our 'What's Shakin Bacon' Bandana. This whimsical and fun accessory is perfect for lighthearted occasions and photo ops. Made from soft and breathable fabric for your pet's comfort. Suitable for dogs of all sizes. Size: 15\" x 15\".",
         "productPrice": "$6.99",
         "productImage": "whats_shakin_bacon_bandana.jpg",
         "category": "bandanas",
@@ -83,7 +83,7 @@ const productData = [
     {
         "sku": "SKU017",
         "productName": "Irish Shamrock Bandana",
-        "productDescription": "Lucky Irish shamrock pattern bandana for St. Patrick's Day.",
+        "productDescription": "Celebrate the luck of the Irish with our Irish Shamrock Bandana. This festive accessory features a lucky shamrock pattern, perfect for St. Patrick's Day or anytime you want to spread some Irish cheer. Made from high-quality fabric for comfort and style. Suitable for dogs of all sizes. Size: 20\" x 20\".",
         "productPrice": "$8.99",
         "productImage": "irish_shamrock_bandana.jpg",
         "category": "bandanas",
@@ -92,7 +92,7 @@ const productData = [
     {
         "sku": "SKU018",
         "productName": "Birthday Girl Bandana",
-        "productDescription": "Celebratory 'Birthday Girl' bandana for a special day.",
+        "productDescription": "Make your pet's special day even more memorable with our 'Birthday Girl' Bandana. This celebratory accessory adds a festive touch to birthday parties and gatherings. Crafted from soft and breathable fabric for your pet's comfort. Suitable for dogs of all sizes. Size: 14\" x 14\".",
         "productPrice": "$6.99",
         "productImage": "birthday_girl_bandana.jpg",
         "category": "bandanas",
@@ -101,7 +101,7 @@ const productData = [
     {
         "sku": "SKU019",
         "productName": "Black and White Plaid Bandana",
-        "productDescription": "Timeless black and white plaid pattern bandana.",
+        "productDescription": "Embrace classic style with our Black and White Plaid Bandana. This timeless pattern adds a touch of sophistication to your pet's look. Made from high-quality fabric for comfort and durability. Suitable for dogs of all sizes. Size: 18\" x 18\".",
         "productPrice": "$7.99",
         "productImage": "black_white_plaid_bandana.jpg",
         "category": "bandanas",
@@ -110,7 +110,7 @@ const productData = [
     {
         "sku": "SKU020",
         "productName": "Watermelon Pattern Bandana",
-        "productDescription": "Refreshing watermelon pattern bandana for a fruity summer.",
+        "productDescription": "Stay cool and refreshing with our Watermelon Pattern Bandana. Perfect for summer adventures, this fruity accessory adds a playful vibe to your pet's ensemble. Made from soft and breathable fabric for comfort. Suitable for dogs of all sizes. Size: 19\" x 19\".",
         "productPrice": "$10.99",
         "productImage": "watermelon_pattern_bandana.jpg",
         "category": "bandanas",
@@ -119,7 +119,7 @@ const productData = [
     {
         "sku": "SKU021",
         "productName": "Ice Cream Lover Bandana",
-        "productDescription": "Sweet 'Ice Cream Lover' bandana for dessert enthusiasts.",
+        "productDescription": "Satisfy your pet's sweet tooth with our 'Ice Cream Lover' Bandana. This adorable accessory is perfect for dessert enthusiasts and adds a playful touch to your pet's look. Made from soft and breathable fabric for your pet's comfort. Suitable for dogs of all sizes. Size: 16\" x 16\".",
         "productPrice": "$8.99",
         "productImage": "ice_cream_lover_bandana.jpg",
         "category": "bandanas",
@@ -128,7 +128,7 @@ const productData = [
     {
         "sku": "SKU023",
         "productName": "Red Leather Collar",
-        "productDescription": "Stylish red leather collar for your furry friend.",
+        "productDescription": "Make a bold statement with our Red Leather Collar. Crafted from genuine leather, this collar exudes sophistication and durability. The vibrant red color adds a pop of style to your pet's appearance. Features an adjustable buckle for a customized fit. Suitable for medium to large dogs. Size: 16\" - 22\".",
         "productPrice": "$15.99",
         "productImage": "red_leather_collar.jpg",
         "category": "collars",
@@ -137,7 +137,7 @@ const productData = [
     {
         "sku": "SKU024",
         "productName": "Floral Collar",
-        "productDescription": "Charming floral pattern collar for a fashionable pet.",
+        "productDescription": "Indulge your pet in floral charm with our Floral Collar. This delightful collar features a charming floral pattern that suits any playful pup. Made from soft and durable material, it provides both comfort and style. Complete with a D-ring for leash attachment. Suitable for small to medium-sized dogs. Size: 12\" - 18\".",
         "productPrice": "$12.99",
         "productImage": "floral_collar.jpg",
         "category": "collars",
@@ -146,7 +146,7 @@ const productData = [
     {
         "sku": "SKU025",
         "productName": "Light Blue Collar",
-        "productDescription": "Light blue collar for a simple and elegant look.",
+        "productDescription": "Embrace simplicity with our Light Blue Collar. The light blue color offers a refreshing and stylish look for your pet. Crafted with adjustable sizing, it ensures a comfortable fit for your furry companion. Features a secure buckle closure. Suitable for dogs of all sizes. Size: 10\" - 20\".",
         "productPrice": "$9.99",
         "productImage": "light_blue_collar.jpg",
         "category": "collars",
@@ -155,7 +155,7 @@ const productData = [
     {
         "sku": "SKU026",
         "productName": "Brown Leather Collar",
-        "productDescription": "Classic brown leather collar for a timeless style.",
+        "productDescription": "Experience elegance and durability with our Brown Leather Collar. Made from genuine leather, this collar combines style and functionality. The rich brown color exudes sophistication, while the sturdy construction ensures reliability. Features a loop handle for comfortable grip. Suitable for medium to large dogs. Size: 14\" - 20\".",
         "productPrice": "$14.99",
         "productImage": "brown_leather_collar.jpg",
         "category": "collars",
@@ -164,7 +164,7 @@ const productData = [
     {
         "sku": "SKU027",
         "productName": "Tennis Ball Collar",
-        "productDescription": "Playful tennis ball pattern collar for an active pup.",
+        "productDescription": "Play fetch in style with our Tennis Ball Collar. This playful accessory features a tennis ball pattern that's perfect for active dogs. Made from high-quality fabric for comfort and durability. Complete with a D-ring for leash attachment. Suitable for dogs of all sizes. Size: 16\" - 22\".",
         "productPrice": "$11.99",
         "productImage": "tennis_ball_collar.jpg",
         "category": "collars",
@@ -173,16 +173,16 @@ const productData = [
     {
         "sku": "SKU028",
         "productName": "Black and White Striped Collar",
-        "productDescription": "Bold black and white striped collar for a stylish pet.",
-        "productPrice": "$13.99",
+        "productDescription": "Make a bold statement with our Black and White Striped Collar. The striking striped pattern adds a touch of contemporary style to your pet's look. Made from soft and breathable fabric for comfort. Suitable for dogs of all sizes. Size: 18\" - 24\".",
+        "productPrice": "$10.99",
         "productImage": "black_white_striped_collar.jpg",
         "category": "collars",
-        "url": "black-and-white-striped-collar"
+        "url": "black-white-striped-collar"
     },
     {
         "sku": "SKU029",
         "productName": "Dark Blue Leash",
-        "productDescription": "Stylish dark blue leash for your furry friend.",
+        "productDescription": "Enhance your daily walks with our Dark Blue Leash. Made from durable nylon material, this leash offers both strength and style. The dark blue color adds a touch of elegance to your pet's outings. Features a comfortable handle and a sturdy metal clasp. Length: 5 feet.",
         "productPrice": "$18.99",
         "productImage": "dark_blue_leash.jpg",
         "category": "leashes",
@@ -191,7 +191,7 @@ const productData = [
     {
         "sku": "SKU030",
         "productName": "Light Blue Leash",
-        "productDescription": "Light blue leash for a coordinated look.",
+        "productDescription": "Coordinate your pet's look with our Light Blue Leash. Crafted from soft and durable material, this leash provides a comfortable grip for pet owners. The light blue color adds a pop of style to your daily walks. Equipped with a sturdy metal clasp for secure attachment. Length: 6 feet.",
         "productPrice": "$17.99",
         "productImage": "light_blue_leash.jpg",
         "category": "leashes",
@@ -200,7 +200,7 @@ const productData = [
     {
         "sku": "SKU031",
         "productName": "Brown Leather Leash",
-        "productDescription": "Classic brown leather leash for a timeless style.",
+        "productDescription": "Experience elegance and durability with our Brown Leather Leash. Made from genuine leather, this leash combines style and functionality. The rich brown color exudes sophistication, while the sturdy construction ensures reliability. Features a loop handle for comfortable grip. Length: 4 feet.",
         "productPrice": "$21.99",
         "productImage": "brown_leather_leash.jpg",
         "category": "leashes",
@@ -209,8 +209,8 @@ const productData = [
     {
         "sku": "SKU032",
         "productName": "Red Striped Leash",
-        "productDescription": "Bold red striped leash for a statement look.",
-        "productPrice": "$19.99",
+        "productDescription": "Add a pop of color to your walks with our Red Striped Leash. The vibrant red stripes bring energy and style to your pet's outings. Made from durable nylon material for long-lasting use. Features a comfortable handle and a strong metal clasp. Length: 5 feet.",
+        "productPrice": "$15.99",
         "productImage": "red_striped_leash.jpg",
         "category": "leashes",
         "url": "red-striped-leash"
@@ -218,8 +218,8 @@ const productData = [
     {
         "sku": "SKU033",
         "productName": "Geometric Leash",
-        "productDescription": "Modern geometric pattern leash for a trendy pet.",
-        "productPrice": "$16.99",
+        "productDescription": "Elevate your pet's style with our Geometric Leash. The intricate geometric pattern adds a modern and artistic touch to your walks. Crafted from durable material for strength and longevity. Features a comfortable handle and a secure metal clasp. Length: 6 feet.",
+        "productPrice": "$19.99",
         "productImage": "geometric_leash.jpg",
         "category": "leashes",
         "url": "geometric-leash"
@@ -227,12 +227,12 @@ const productData = [
     {
         "sku": "SKU034",
         "productName": "Navy Blue Leash",
-        "productDescription": "Navy blue leash for a classic and versatile look.",
-        "productPrice": "$18.99",
+        "productDescription": "Sail away in style with our Navy Blue Leash. The deep blue color evokes a sense of nautical adventure. Made from high-quality material for durability and comfort. Features a loop handle and a sturdy metal clasp. Length: 5 feet.",
+        "productPrice": "$16.99",
         "productImage": "navy_blue_leash.jpg",
         "category": "leashes",
         "url": "navy-blue-leash"
-    },
+    }
 ];
 
 export default productData;
