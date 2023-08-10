50fa9c3d41169a79cff283894f8fadf83db6ae16
Kars Rao <kars@predikkta.com>
2023-08-10 19:34:03 +1000

    updated productData with urls

--- src/productData.js
+++ src/productData.js
@@ -1,11 +1,12 @@
-const productData = [    
+const productData = [
     {
         "sku": "SKU001",
         "productName": "Classic Paisley Bandana",
         "productDescription": "Timeless paisley pattern bandana for a versatile look.",
         "productPrice": "$9.99",
         "productImage": "paisley-bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "classic-paisley-bandana"
     },
     {
         "sku": "SKU004",
@@ -13,7 +14,8 @@ const productData = [
         "productDescription": "Delicate floral pattern bandana for a feminine look.",
         "productPrice": "$7.99",
         "productImage": "floral-bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "floral-bandana"
     },
     {
         "sku": "SKU006",
@@ -21,7 +23,8 @@ const productData = [
         "productDescription": "Retro-style paisley pattern bandana for a classic vibe.",
         "productPrice": "$11.99",
         "productImage": "vintage-bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "vintage-paisley-bandana"
     },
     {
         "sku": "SKU011",
@@ -29,7 +32,8 @@ const productData = [
         "productDescription": "Classic red plaid bandana for a timeless look.",
         "productPrice": "$9.99",
         "productImage": "red_plaid_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "red-plaid-bandana"
     },
     {
         "sku": "SKU012",
@@ -37,7 +41,8 @@ const productData = [
         "productDescription": "Fun Pokemon-themed bandana for fans of the franchise.",
         "productPrice": "$14.99",
         "productImage": "pokemon_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "pokemon-bandana"
     },
     {
         "sku": "SKU013",
@@ -45,7 +50,8 @@ const productData = [
         "productDescription": "Vibrant Mexican-themed bandana for a festive look.",
         "productPrice": "$11.99",
         "productImage": "mexican_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "mexican-fiesta-bandana"
     },
     {
         "sku": "SKU014",
@@ -53,7 +59,8 @@ const productData = [
         "productDescription": "Unique green spotted pattern bandana for a playful look.",
         "productPrice": "$8.99",
         "productImage": "green_spotted_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "green-spotted-bandana"
     },
     {
         "sku": "SKU015",
@@ -61,7 +68,8 @@ const productData = [
         "productDescription": "Fresh orange fruit pattern bandana for a fruity vibe.",
         "productPrice": "$9.99",
         "productImage": "orange_fruit_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "orange-fruit-bandana"
     },
     {
         "sku": "SKU016",
@@ -69,7 +77,8 @@ const productData = [
         "productDescription": "Whimsical 'What's Shakin Bacon' bandana for a fun twist.",
         "productPrice": "$6.99",
         "productImage": "whats_shakin_bacon_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "whats-shakin-bacon-bandana"
     },
     {
         "sku": "SKU017",
@@ -77,7 +86,8 @@ const productData = [
         "productDescription": "Lucky Irish shamrock pattern bandana for St. Patrick's Day.",
         "productPrice": "$8.99",
         "productImage": "irish_shamrock_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "irish-shamrock-bandana"
     },
     {
         "sku": "SKU018",
@@ -85,7 +95,8 @@ const productData = [
         "productDescription": "Celebratory 'Birthday Girl' bandana for a special day.",
         "productPrice": "$6.99",
         "productImage": "birthday_girl_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "birthday-girl-bandana"
     },
     {
         "sku": "SKU019",
@@ -93,7 +104,8 @@ const productData = [
         "productDescription": "Timeless black and white plaid pattern bandana.",
         "productPrice": "$7.99",
         "productImage": "black_white_plaid_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "black-and-white-plaid-bandana"
     },
     {
         "sku": "SKU020",
@@ -101,7 +113,8 @@ const productData = [
         "productDescription": "Refreshing watermelon pattern bandana for a fruity summer.",
         "productPrice": "$10.99",
         "productImage": "watermelon_pattern_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "watermelon-pattern-bandana"
     },
     {
         "sku": "SKU021",
@@ -109,7 +122,8 @@ const productData = [
         "productDescription": "Sweet 'Ice Cream Lover' bandana for dessert enthusiasts.",
         "productPrice": "$8.99",
         "productImage": "ice_cream_lover_bandana.jpg",
-        "category": "bandanas"
+        "category": "bandanas",
+        "url": "ice-cream-lover-bandana"
     },
     {
         "sku": "SKU023",
@@ -117,7 +131,8 @@ const productData = [
         "productDescription": "Stylish red leather collar for your furry friend.",
         "productPrice": "$15.99",
         "productImage": "red_leather_collar.jpg",
-        "category": "collars"
+        "category": "collars",
+        "url": "red-leather-collar"
     },
     {
         "sku": "SKU024",
@@ -125,7 +140,8 @@ const productData = [
         "productDescription": "Charming floral pattern collar for a fashionable pet.",
         "productPrice": "$12.99",
         "productImage": "floral_collar.jpg",
-        "category": "collars"
+        "category": "collars",
+        "url": "floral-collar"
     },
     {
         "sku": "SKU025",
@@ -133,7 +149,8 @@ const productData = [
         "productDescription": "Light blue collar for a simple and elegant look.",
         "productPrice": "$9.99",
         "productImage": "light_blue_collar.jpg",
-        "category": "collars"
+        "category": "collars",
+        "url": "light-blue-collar"
     },
     {
         "sku": "SKU026",
@@ -141,23 +158,26 @@ const productData = [
         "productDescription": "Classic brown leather collar for a timeless style.",
         "productPrice": "$14.99",
         "productImage": "brown_leather_collar.jpg",
-        "category": "collars"
+        "category": "collars",
+        "url": "brown-leather-collar"
     },
     {
         "sku": "SKU027",
         "productName": "Tennis Ball Collar",
-        "productDescription": "Playful tennis ball pattern collar for an active pet.",
+        "productDescription": "Playful tennis ball pattern collar for an active pup.",
         "productPrice": "$11.99",
         "productImage": "tennis_ball_collar.jpg",
-        "category": "collars"
+        "category": "collars",
+        "url": "tennis-ball-collar"
     },
     {
         "sku": "SKU028",
         "productName": "Black and White Striped Collar",
-        "productDescription": "Bold black and white striped collar for a stylish look.",
+        "productDescription": "Bold black and white striped collar for a stylish pet.",
         "productPrice": "$13.99",
         "productImage": "black_white_striped_collar.jpg",
-        "category": "collars"
+        "category": "collars",
+        "url": "black-and-white-striped-collar"
     },
     {
         "sku": "SKU029",
@@ -165,7 +185,8 @@ const productData = [
         "productDescription": "Stylish dark blue leash for your furry friend.",
         "productPrice": "$18.99",
         "productImage": "dark_blue_leash.jpg",
-        "category": "leashes"
+        "category": "leashes",
+        "url": "dark-blue-leash"
     },
     {
         "sku": "SKU030",
@@ -173,7 +194,8 @@ const productData = [
         "productDescription": "Light blue leash for a coordinated look.",
         "productPrice": "$17.99",
         "productImage": "light_blue_leash.jpg",
-        "category": "leashes"
+        "category": "leashes",
+        "url": "light-blue-leash"
     },
     {
         "sku": "SKU031",
@@ -181,7 +203,8 @@ const productData = [
         "productDescription": "Classic brown leather leash for a timeless style.",
         "productPrice": "$21.99",
         "productImage": "brown_leather_leash.jpg",
-        "category": "leashes"
+        "category": "leashes",
+        "url": "brown-leather-leash"
     },
     {
         "sku": "SKU032",
@@ -189,7 +212,8 @@ const productData = [
         "productDescription": "Bold red striped leash for a statement look.",
         "productPrice": "$19.99",
         "productImage": "red_striped_leash.jpg",
-        "category": "leashes"
+        "category": "leashes",
+        "url": "red-striped-leash"
     },
     {
         "sku": "SKU033",
@@ -197,7 +221,8 @@ const productData = [
         "productDescription": "Modern geometric pattern leash for a trendy pet.",
         "productPrice": "$16.99",
         "productImage": "geometric_leash.jpg",
-        "category": "leashes"
+        "category": "leashes",
+        "url": "geometric-leash"
     },
     {
         "sku": "SKU034",
@@ -205,8 +230,9 @@ const productData = [
         "productDescription": "Navy blue leash for a classic and versatile look.",
         "productPrice": "$18.99",
         "productImage": "navy_blue_leash.jpg",
-        "category": "leashes"
+        "category": "leashes",
+        "url": "navy-blue-leash"
     },
-]
+];
 
-export default productData
+export default productData;
