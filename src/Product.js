5a750bd1374ac1006f7541eca9916eee0945b4f5
Kars Rao <kars@predikkta.com>
2023-08-10 20:28:31 +1000

    small style update and code tidy

--- src/Product.js
+++ src/Product.js
@@ -1,17 +1,14 @@
-// import { useState } from 'react'
 import './Product.css'
 
 import { Grid } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 const Product = ({ items }) => {
-  // const [selectedProduct, setSelectedProduct] = useState("")
   return (
     <>
       {items.map((prod, index) => {
         return (
           <Grid item xs={12} md={4} key={index} className='product-thumbnail'>
-            {/* <Link to={"/products/"+prod.category+"/"+prod.url} onClick={() => setSelectedProduct(prod)}> */}
             <Link to={'/products/' + prod.category + '/' + prod.url}>
               <img src={'/images/' + prod.category + '/' + prod.productImage} alt={prod.productName} className='product-image' />
               <div className="name-text">{prod.productName}</div>
@@ -23,6 +20,4 @@ const Product = ({ items }) => {
     </>
   )
 }
-
-
 export default Product
