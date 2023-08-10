7823ea095dd1c2439da5633fa4075529b58acf0e
Kars Rao <kars@predikkta.com>
2023-08-10 19:35:18 +1000

    working on product details page

--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,21 @@
-import React from 'react'
+// import { useState } from 'react'
 import './Product.css'
 
 import { Grid } from '@mui/material'
+import { Link } from 'react-router-dom'
 
-const Product = ({ items }) => {            
+const Product = ({ items }) => {
+  // const [selectedProduct, setSelectedProduct] = useState("")
   return (
     <>
-      {items.map((prod) => {
+      {items.map((prod, index) => {
         return (
-          <Grid item xs={12} md={4} key={prod.index} className='product-thumbnail'>          
-            <img src={'/images/' + prod.category + '/' + prod.productImage} alt={prod.productName} className='product-image' />
-            <div className="name-text">{prod.productName}</div>
+          <Grid item xs={12} md={4} key={index} className='product-thumbnail'>
+            {/* <Link to={"/products/"+prod.category+"/"+prod.url} onClick={() => setSelectedProduct(prod)}> */}
+            <Link to={'/products/' + prod.category + '/' + prod.url}>
+              <img src={'/images/' + prod.category + '/' + prod.productImage} alt={prod.productName} className='product-image' />
+              <div className="name-text">{prod.productName}</div>
+            </Link>
             <div className='price-text'>{prod.productPrice}</div>
           </Grid>
         )
--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -1,7 +1,17 @@
+import { useParams } from "react-router-dom";
+
+// import productData from "./productData";
+
 function ProductDetail(){
+    const params = useParams()
+    const paramUrl = params.product
+    // const foundUrl = productData.some(el => el.url === paramUrl)
+
+    console.log(paramUrl)
+
     return(
         <>
-            
+            <p>This is a product detail page</p>
         </>
     )
 }
